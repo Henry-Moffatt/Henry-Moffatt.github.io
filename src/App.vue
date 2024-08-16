e54189fdd12cfdc504b7c59e34b062a340bf5cb0
HenryM <hmoffatt.2025@trinity.nsw.edu.au>
2024-08-16 11:13:09 +1000

    Initial Commit - Laying Foundations

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue'
+import CrimeBox from "@/components/crimeBox.vue";
+let data= ref(["Loading...", "Loading...", "Loading...", "Loading...", "Loading...", "Loading..."])
+fetch('https://data.cityofchicago.org/resource/ijzp-q8t2.json?$limit=100')
+    .then(async (d) => {data.value = await d.json()})
+    .catch((error) => {data.value = "Error fetching data"})
+
+</script>
+
+<template>
+    <div class="grid-container">
+      <crime-box class="box" :data="data[0]" style="grid-column:1; grid-row:1;"></crime-box>
+      <crime-box class="box" :data="data[1]" style="grid-column:2; grid-row:1;"></crime-box>
+      <crime-box class="box" :data="data[2]" style="grid-column:3; grid-row:1;"></crime-box>
+      <crime-box class="box" :data="data[3]" style="grid-column:1; grid-row:2;"></crime-box>
+      <crime-box class="box" :data="data[4]" style="grid-column:2; grid-row:2;"></crime-box>
+      <crime-box class="box" :data="data[5]" style="grid-column:3; grid-row:2;"></crime-box>
+    </div>
+</template>
+
+
---
+++ src/components/crimeBox.vue
@@ -0,0 +1,32 @@
+<script setup>
+import {ref} from 'vue'
+
+const props = defineProps(['data'])
+let finData = ref({id:"Loading ID",case_number:"Loading Case Number",date:"Loading",block:"Loading Block",iucr:"Loading IUCR",primary_type:"Loading Primary Type",description:"Loading Description",location_description:"Loading Location Description",arrest:false,domestic:false,beat:"Loading Beat",district:"Loading District",ward:"Loading Ward",community_area:"Loading Community Area",fbi_code:"Loading FBI Code",year:"Loading Year",updated_on:"Loading Last Update"})
+if (typeof props.data == "string") {
+}
+else {
+  finData.value = props.data
+}
+
+</script>
+
+<template>
+  <div class="crime">
+    <h1>{{finData.primary_type}}</h1>
+    <h2>{{finData.description}}</h2>
+    <p>{{finData.location_description}}</p>
+    <p>Date: {{finData.date.slice(0,10)}}</p>
+  </div>
+
+</template>
+
+<style>
+.crime {
+  border: 5px solid rgba(255,210,255,1);
+  border-radius: 10px;
+  margin: 10px;
+  background: rgba(255,220,255,1);
+  padding: 5%
+}
+</style>
